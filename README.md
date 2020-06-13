792e5fdd3530ba65ff2a5fe192a2d98bc792c373
MathsPrado <matheusprado55@gmail.com>
2020-06-13 14:23:01 -0300

    Add migrations, initial initial charge, configuration Context add Sqlite

--- README.md
+++
@@ -1,2 +0,0 @@
-# Web-API
-https://www.udemy.com/course/criando-web-api-com-aspnet-core-31-ef-core-31/
---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Data;
+using SmartSchool.API.Models;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        private readonly SmartContext _context;
+
+        public AlunoController(SmartContext context)
+        {
+            _context = context;
+        }      
+
+        // GET: api/Aluno
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Alunos);
+        }
+
+
+        // GET: api/Aluno/5
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+
+            if(aluno == null)
+            {
+                return BadRequest("Aluno não encontrado");
+            }
+            return Ok(aluno);
+        }
+
+
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome, string Sobrenome)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(Sobrenome));
+            if (aluno == null)
+            {
+                return BadRequest("Aluno não encontrado");
+            }
+            return Ok(aluno);
+        }
+
+
+
+
+        // POST: api/Aluno
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+
+        // PUT: api/Aluno/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+    }
+}
