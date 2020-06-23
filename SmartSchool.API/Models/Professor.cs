e227a517bf850e7b68d50f0f00092268d166956c
MathsPrado <matheusprado55@gmail.com>
2020-06-23 20:42:01 -0300

    Alter model Aluno and Professor

--- SmartSchool.API/Models/Professor.cs
+++ SmartSchool.API/Models/Professor.cs
@@ -8,14 +8,19 @@ namespace SmartSchool.API.Models
     public class Professor
     {
         public int Id { get; set; }
+        public int Registro { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
+        public DateTime DataInicio { get; set; } = DateTime.Now;
+        public DateTime? DateFim { get; set; } = null;
+        public bool Ativo { get; set; } = true;
         public IEnumerable<Disciplina> Disciplinas { get; set; }
 
-        public Professor(int id, string nome)
+        public Professor(int id, string nome, int registro)
         {
             Id = id;
             Nome = nome;
+            Registro = registro;
         }
 
         public Professor()
